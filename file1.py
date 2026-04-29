6cf8f40e714e59c495a5a81ff11211ab6e8d4fd2
narayanareddy999 <narayanareddy7129@gmail.com>
2026-04-29 16:40:11 +0530

    'final_version'

--- file1.py
+++ file1.py
@@ -1,10 +1,17 @@
 import streamlit as st
+import cv2
 
 st.title('Machine Learning')
 st.write('Ram') 
-<<<<<<< HEAD
+
 st.write('SitaRam') 
 st.write('Deep Learning')
-=======
 st.image("img2.jpg")
->>>>>>> 92592c3
+
+import cv2 
+img2 =cv2.imread("doremon.webp")
+
+height = st.slider("select the Height",100,500)
+width = st.slider("select the width",100,500)
+
+img = cv2.resize(img2,(width,height))
